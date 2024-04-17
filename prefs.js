9234defc59bf0675cff7ef975a3f98843466263d
Jordan MacDonald <jordan@wastedintelligence.com>
2024-04-16 22:12:37 -0400

    Extract appearance settings into a separate method

--- prefs.js
+++ prefs.js
@@ -13,11 +13,14 @@ export default class ScratchpadPreferences extends ExtensionPreferences {
         });
         window.add(page);
 
+        page.add(this._appearanceGroup());
+    }
+
+    _appearanceGroup() {
         const group = new Adw.PreferencesGroup({
             title: _('Appearance'),
             description: _('Configure the appearance of scratchpad windows'),
         });
-        page.add(group);
 
         // Create scratchpad window dimension configuration rows
         const widthRow = new Adw.EntryRow({
@@ -30,10 +33,12 @@ export default class ScratchpadPreferences extends ExtensionPreferences {
         group.add(heightRow);
 
         // Binding row inputs to settings object
-        window._settings = this.getSettings();
-        window._settings.bind('window-width', widthRow, 'text',
+        const settings = this.getSettings();
+        settings.bind('window-width', widthRow, 'text',
             Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('window-height', heightRow, 'text',
+        settings.bind('window-height', heightRow, 'text',
             Gio.SettingsBindFlags.DEFAULT);
+
+        return group;
     }
 }
