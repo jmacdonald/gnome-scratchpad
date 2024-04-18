319058ca62c0976cfdd61d1be87871747f6d8516
Jordan MacDonald <jordan@wastedintelligence.com>
2024-04-17 20:54:54 -0400

    Remove preferences

--- extension.js
+++ extension.js
@@ -1,32 +1,11 @@
-import St from 'gi://St';
-
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-
 
 export default class ScratchpadExtension extends Extension {
     enable() {
         console.warn("enabling scratchpad extension");
-        this._settings = this.getSettings();
-        this._settings.connect('changed::window-width', (settings, key) => {
-            console.warn(`${key} = ${this.window_width().print(true)}`);
-        });
-        this._settings.connect('changed::window-height', (settings, key) => {
-            console.warn(`${key} = ${this.window_height().print(true)}`);
-        });
-    }
-
-    window_width() {
-        return this._settings.get_value('window-width');
-    }
-
-    window_height() {
-        return this._settings.get_value('window-height');
     }
 
     disable() {
         console.warn("disabling scratchpad extension");
-        this._settings = null;
     }
 }
